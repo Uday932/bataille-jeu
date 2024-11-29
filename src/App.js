46de5b25b9aeb3a00f38f68066260c7e88f60f59
Uday932 <uday.bhavsar.sio@gmail.com>
2024-11-29 10:57:36 +0100

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Simple Bataille</h1>
+      <div className="game-container">
+        <div className="player-area">
+          <h2>Votre carte</h2>
+        </div>
+        <div className="computer-area">
+          <h2>Carte de l'Ordinateur</h2>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
